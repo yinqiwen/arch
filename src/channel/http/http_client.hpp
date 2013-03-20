a12368d91380edcefbc1fb918c80eeb4329b6c26
yinqiwen <yinqiwen@gmail.com>
2013-03-20 16:44:39 +0800

    rm potential stackoverflow in logger

--- src/channel/http/http_client.hpp
+++ src/channel/http/http_client.hpp
@@ -14,24 +14,43 @@
 #include <vector>
 #include <string>
 #include "http_message.hpp"
+#include "channel/channel_service.hpp"
 
 using arch::buffer::Buffer;
+using namespace arch::channel;
 namespace arch
 {
-    namespace channel
-    {
-        namespace http
-        {
-        	   class HttpClient{
-        		   private:
-        			   Channel* GetIdleConnection(const char* host);
-        		   public:
-        			   bool Do(HTTPMessage& req);
-        			   bool Get(const std::string& url);
-        	   };
-        }
-    }
+	namespace channel
+	{
+		namespace http
+		{
+			struct HttpClientOptions
+			{
+					int max_idle_conns_per_host;
+			};
+			struct HttpClientCallback
+			{
+					virtual void OnResponse(HTTPMessage& response) = 0;
+					virtual void OnBody(Buffer& content) = 0;
+					virtual void OnComplete() = 0;
+					virtual void OnError(int errorCode, const std::string& cause) = 0;
+					virtual ~HttpClientCallback()
+					{
+					}
+			};
+			class HttpClient
+			{
+				private:
+					HttpClientOptions m_options;
+					ChannelService& m_serv;
+					Channel* GetIdleConnection(const char* host);
+				public:
+					HttpClient(ChannelService& serv);
+					bool Do(HTTPMessage& req, HttpClientCallback* cb);
+					bool Get(const std::string& url, HttpClientCallback* cb);
+			};
+		}
+	}
 }
-
 
 #endif /* HTTP_CLIENT_HPP_ */
